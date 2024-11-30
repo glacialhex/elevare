850c72f5c17deb2d2f34cbc5c902717f72827ac6
Yosif Ibrahim <65606350+Uoerim@users.noreply.github.com>
2024-11-30 22:13:58 +0200

    manually getting the env variables

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,24 +1,35 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import pg from 'pg';
 const { Pool } = pg;
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env.development.local' });
 
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { username, password } = req.body;
 
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Username and password are required' });
+    }
+
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT token FROM elevare_users WHERE username = $1 AND password = $2', [username, password]);
-      client.release();
-
-      if (result.rows.length > 0) {
-        res.status(200).json({ id: result.rows[0].id });
-      } else {
-        res.status(404).json({ error: 'User not found' });
+      try {
+        const result = await client.query('SELECT id FROM elevare_users WHERE username = $1 AND password = $2', [username, password]);
+        if (result.rows.length > 0) {
+          res.status(200).json({ id: result.rows[0].id });
+        } else {
+          res.status(404).json({ error: 'User not found' });
+        }
+      } finally {
+        client.release();
       }
     } catch (error) {
       console.error('Database query error:', error);
